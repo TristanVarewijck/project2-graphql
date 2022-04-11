87a73b794ccd8bf816a7e4c9c3d38d66f38f1200
TristanVarewijck <tristanconnect4@outlook.com>
2022-04-11 17:08:23 +0200

    pop-up:

--- scripts/data/cleaning.js
+++ scripts/data/cleaning.js
@@ -3,34 +3,20 @@ const launchesSection = document.getElementById("launchSection");
 const cleanData = (data) => {
   let items = data.data.launches;
   console.log(items);
-
-  //   query {
-  //     launches {
-  //       details
-  //       launch_date_utc
-  //       launch_site {
-  //         site_name
-  //       }
-  //       launch_success
-  //       rocket {
-  //         rocket_name
-  //       }
-  //     }
-  //   }
-
   items.map((item) => {
     //   CLEANING
     item.launch_date_utc = parseDate(item.launch_date_utc);
     item.links.flickr_images[0] = hasImage(item.links.flickr_images[0]);
+    item.details = hasDetails(item.details);
 
     const launchesContents = `
-      <div>
+      <article>
       <small>${item.launch_date_utc}</small>
       <h1>${item.rocket.rocket_name}</h1>
+      <small> Launch site: <span>${item.launch_site.site_name}</span></small>
       <img src=${item.links.flickr_images[0]} alt=${item.launch_site.site_name} width="150px"/>
       <p>${item.details}</p>
-   
-      </div>      
+      </article>      
 `;
     const launchContainer = document.createElement("li");
     launchContainer.innerHTML = launchesContents;
@@ -48,8 +34,13 @@ function parseDate(launchDate) {
 }
 
 function hasImage(image) {
-  image ? (image = image) : (image = "../images/placeholder.png");
+  image ? (image = image) : (image = "../assets/images/placeholder.png");
   return image;
+}
+
+function hasDetails(details) {
+  details ? (details = details) : (details = "-");
+  return details;
 }
 
 export { cleanData };
