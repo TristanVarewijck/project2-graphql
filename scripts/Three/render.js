a1bdd165969fb3ed4c9fad262f444dd26671008e
Daan Ketelaars <daanketelaars@gmail.com>
2022-04-13 13:48:39 +0200

    changes threejs scene

--- scripts/Three/render.js
+++ scripts/Three/render.js
@@ -1,8 +1,14 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
-import {GLTFLoader}  from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
+import {
+    GLTFLoader
+} from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 import * as dat from '../../node_modules/lil-gui/dist/lil-gui.esm.js';
 
 export function renderMap() {
+
+    const button = document.querySelector('button');
+    console.log(button);
+
     const canvas = document.querySelector('canvas.webgl')
 
     const gui = new dat.GUI()
@@ -12,84 +18,121 @@ export function renderMap() {
         width: window.innerWidth,
         height: window.innerHeight
     }
-    
+
     // Scene
     const scene = new THREE.Scene()
-    scene.background = new THREE.Color( 0x1f1e1e );
+    scene.background = new THREE.Color(0x1f1e1e);
     let USAMesh
     let rocket
 
     // Textures
     const textureLoader = new THREE.TextureLoader()
-    const plasticColor = textureLoader.load('/assets/textures/Plastic_001_COLOR.jpg')
-    const plasticRoughness = textureLoader.load('/assets/textures/Plastic_001_ROUGH.jpg')
-    const rocketMatcap = textureLoader.load('/assets/matcap.png')
+
+    // Fog
+    const fog = new THREE.Fog('#111111', 1, 13)
+    scene.fog = fog
 
 
     // Objects
     const manager = new THREE.LoadingManager();
     const loader = new GLTFLoader(manager);
 
-    loader.load( '../../assets/usa.gltf', function ( gltf ) {
+    loader.load('../../assets/usa.gltf', function (gltf) {
         USAMesh = gltf.scene
-        USAMesh.position.x = 1 
+        USAMesh.position.x = 1
         USAMesh.position.y = 1
         USAMesh.position.z = 1
 
         camera.lookAt(USAMesh.position)
-        scene.add( USAMesh );   
+        scene.add(USAMesh);
 
-    }, undefined, function ( error ) {
+    }, undefined, function (error) {
 
-	    console.error( error );
+        console.error(error);
 
     });
 
-    loader.load( '../../assets/rocket.gltf', function ( gltf ) {
+    loader.load('../../assets/test.gltf', function (gltf) {
         rocket = gltf.scene
-        rocket.position.x = 4.3
-        rocket.position.y = 2
-        rocket.position.z = 3.2
-        rocket.rotation.y = Math.PI / 1.2
+        const launchSite01 = [4.3, 1.4, 3.2]
+        const launchSite02 = [4, 1.4, 2.8]
+        const launchSite03 = [-2.2, 1.4, 1.6]
 
-        rocket.scale.set(0.005, 0.005, 0.005)
+        rocket.position.set(launchSite01[0], launchSite01[1], launchSite01[2])
+        console.log(launchSite01.map(x => x))
+
+
+
+        rocket.rotation.y = Math.PI / 1.2
+        rocket.traverse(n => {
+            if (n.isMesh) {
+                n.castShadow = true
+                n.receiveShadow = true
+                if (n.material.map) n.material.map.anisotropy = 16
+            }
+        })
+
+        rocket.scale.set(0.03, 0.03, 0.03)
         console.log(rocket);
 
-        // animate
-        let succesTl = gsap.timeline();
-
-        succesTl.to(rocket.position, {
-            y: 8,
-            repeat: -1,
-            duration: 5
+        button.addEventListener('click', function () {
+            // animate
+            let succesTl = gsap.timeline();
+            succesTl.to(rocket.position, {
+                y: 8,
+                duration: 1.75
+            })
         })
-        scene.add( rocket );
 
-    }, undefined, function ( error ) {
+        scene.add(rocket);
 
-	    console.error( error );
+    }, undefined, function (error) {
+
+        console.error(error);
 
     });
 
-    const geometry = new THREE.CylinderGeometry( 0.2, 0.2, 0.05, 64 );
-    const material = new THREE.MeshStandardMaterial( {
-        map: plasticColor,
-        roughnessMap: plasticRoughness,
-        metalness: 0.1,
-        roughness: 0
+    const geometry = new THREE.CylinderGeometry(0.2, 0.2, 0.05, 64);
+    const material = new THREE.MeshPhysicalMaterial({
+        color: 0x757575,
+        metalness: 0.9,
+        roughness: 0.1
     });
-    const cylinderFlorida = new THREE.Mesh( geometry, material );
+
+    const material02 = new THREE.MeshPhysicalMaterial({
+        color: 0x757575,
+        metalness: 0.9,
+        roughness: 0.1
+    });
+
+    const material03 = new THREE.MeshPhysicalMaterial({
+        color: 0x757575,
+        metalness: 0.9,
+        roughness: 0.1
+    });
+
+    const cylinderFlorida = new THREE.Mesh(geometry, material);
+    const cylinderFlorida02 = new THREE.Mesh(geometry, material02);
+    const cylinderCalifornia = new THREE.Mesh(geometry, material03);
+
 
     cylinderFlorida.position.x = 4.3
     cylinderFlorida.position.y = 1.38
     cylinderFlorida.position.z = 3.2
 
-    scene.add( cylinderFlorida );
-    
-    
+    cylinderFlorida02.position.x = 4
+    cylinderFlorida02.position.y = 1.4
+    cylinderFlorida02.position.z = 2.8
+
+    cylinderCalifornia.position.x = -2.2
+    cylinderCalifornia.position.y = 1.4
+    cylinderCalifornia.position.z = 1.6
+
+    scene.add(cylinderFlorida, cylinderFlorida02, cylinderCalifornia);
+
+
     // Responsive
-    window.addEventListener('resize', () =>
-    {
+    window.addEventListener('resize', () => {
         // Update sizes
         sizes.width = window.innerWidth
         sizes.height = window.innerHeight
@@ -117,28 +160,30 @@ export function renderMap() {
         y: 0
     }
 
-    window.addEventListener('mousemove', (event) =>
-    {
+    window.addEventListener('mousemove', (event) => {
         cursor.x = event.clientX / sizes.width - 0.5
-        cursor.y = - (event.clientY / sizes.height - 0.5)
+        cursor.y = -(event.clientY / sizes.height - 0.5)
     })
 
 
     // GUI
-    gui.add(camera.position, 'x', - 20, 20, 0.1);
-    gui.add(camera.position, 'y', - 20, 20, 0.1);
-    gui.add(camera.position, 'z', - 20, 20, 0.1);
+    gui.add(camera.position, 'x', -20, 20, 0.1);
+    gui.add(camera.position, 'y', -20, 20, 0.1);
+    gui.add(camera.position, 'z', -20, 20, 0.1);
     scene.add(camera)
 
 
     // Lighting
-    const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x000000, 0.9)
-    scene.add(hemisphereLight)
+    const hemisphereLight = new THREE.HemisphereLight(0xb1d9ff, 0x080820, 1);
+    scene.add(hemisphereLight);
 
-    const light = new THREE.PointLight(0xffffff, 1, 0);
-    light.position.set(5, 50, 10)
-    scene.add(light)
-
+    const light = new THREE.SpotLight(0xffffff, 4);
+    light.position.set(5, 50, 10);
+    light.castShadow = true;
+    light.shadow.bias = -0.0001
+    light.shadow.mapSize.width = 1024 * 4
+    light.shadow.mapSize.height = 1024 * 4
+    scene.add(light);
 
     // Renderer
     const renderer = new THREE.WebGLRenderer({
@@ -149,6 +194,9 @@ export function renderMap() {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     renderer.gammaInput = true
     renderer.gammaOutput = true
+    renderer.toneMapping = THREE.ReinhardToneMapping
+    renderer.toneMappingExposure = 2.3
+    renderer.shadowMap.enabled = true
 
 
     /**
@@ -158,15 +206,18 @@ export function renderMap() {
 
     const clock = new THREE.Clock()
 
-    const tick = () =>
-    {
+    const tick = () => {
         const elapsedTime = clock.getElapsedTime()
 
         // Render
-        camera.position.x = cursor.x 
+        camera.position.x = cursor.x
         camera.position.y = cursor.y + 6
         renderer.render(scene, camera)
-
+        light.position.set(
+            camera.position.x + 10,
+            camera.position.y + 10,
+            camera.position.z + 10
+        )
         // Call tick again on the next frame
         window.requestAnimationFrame(tick)
     }
